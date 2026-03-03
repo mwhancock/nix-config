b43a4f6ad2e639f0de79a564fde69396444209b3
mark <mwhancock@proton.me>
2026-03-03 13:42:07 -0330

    Update: 2026-03-03 13:42

--- configuration.nix
+++ configuration.nix
@@ -1,31 +1,36 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    environment.systemPackages = with pkgs; [
-      git
-      wget
-      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ghostty
-      zed-editor
-      ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ghostty
+    zed-editor
+    nil
+    nixd
+    fastfetch
+  ];
 
-    environment.shells = with pkgs; [ fish ];
-    environment.variables = {
-      EDITOR = "zed";
-      VISUAL = "zed";
-      TERMINAL = "ghostty";
-    };
-
+  environment.shells = with pkgs; [ fish ];
+  environment.variables = {
+    EDITOR = "zed";
+    VISUAL = "zed";
+    TERMINAL = "ghostty";
+  };
 
   # Bootloader.
   boot.loader = {
@@ -35,11 +40,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -49,10 +49,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
@@ -75,71 +71,44 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   hardware.bluetooth = {
-  enable = true;
-  powerOnBoot = true;
-  settings = {
-    General = {
-      Experimental = true;
-      FastConnectable = true;
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
       };
       Policy = {
-      AutoEnable = true;
+        AutoEnable = true;
       };
     };
   };
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mark = {
     isNormalUser = true;
     description = "mark";
     shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" "mark"];
-    packages = with pkgs; [
-
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "mark"
     ];
+
   };
 
   # Programs
-  programs.firefox.enable = true;
   programs.fish.enable = true;
-
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
