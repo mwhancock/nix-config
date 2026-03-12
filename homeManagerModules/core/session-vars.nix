f1be5bb114ce510e801d37f1ade6e64b313ea5fe
mark <mwhancock@proton.me>
2026-03-12 14:47:17 -0230

    Remove XDG_DATA_DIRS from home-manager

--- homeManagerModules/core/session-vars.nix
+++ homeManagerModules/core/session-vars.nix
@@ -5,7 +5,6 @@
     VISUAL = "zed";
     TERMINAL = "ghostty";
     ZEN_PROFILE_DIR = "~/.config/zen zen-backup";
-    XDG_DATA_DIRS = "/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
     XKB_CONFIG_ROOT = "${pkgs.xkeyboard-config}/etc/X11/xkb";
   };
 }
