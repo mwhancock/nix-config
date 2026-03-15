8056e5ecf7d9c6d6797f5b8f449d736164a34303
mark <mwhancock@proton.me>
2026-03-15 12:31:43 -0230

    fix minisforum v3 audio quirk

--- nixModules/hardware/audio.nix
+++ nixModules/hardware/audio.nix
@@ -8,4 +8,34 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  environment.etc."wireplumber/wireplumber.conf.d/alsa-soft-mixer.conf".text = ''
+    monitor.alsa.rules = [
+      {
+        matches = [
+          {
+            device.name = "alsa_card.pci-0000_c4_00.6"
+          }
+        ]
+        actions = {
+          update-props = {
+            api.alsa.soft-mixer = true
+          }
+        }
+      },
+      {
+        matches = [
+          {
+            device.name = "~alsa_card.*"
+            node.name = "~alsa_input.*"
+          }
+        ]
+        actions = {
+          update-props = {
+            api.alsa.soft-mixer = false
+          }
+        }
+      }
+    ]
+  '';
 }
