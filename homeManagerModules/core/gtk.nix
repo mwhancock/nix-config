d9c43baec6a221dbb8b154f1b45cfcede13fdf30
mark <mwhancock@proton.me>
2026-03-15 12:52:00 -0230

    Update: 2026-03-15 12:52

--- homeManagerModules/core/gtk.nix
+++ homeManagerModules/core/gtk.nix
@@ -10,5 +10,18 @@
       name = "Adwaita";
       package = pkgs.gnome-themes-extra;
     };
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+      size = 24;
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 24;
   };
 }
