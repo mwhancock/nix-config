13b129b25a6cee02ed094a93daa84f9bd138e16d
mark <mwhancock@proton.me>
2026-03-15 13:07:59 -0230

    Update: 2026-03-15 13:07

--- nixModules/desktop/gnome.nix
+++ nixModules/desktop/gnome.nix
@@ -2,8 +2,8 @@
 
 {
 
-  services.gnome.core-apps.enable = true;
-  services.gnome.core-developer-tools.enable = true;
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
