7aff5c089310b979610641327ee3b41aba64061d
mark <mwhancock@proton.me>
2026-03-24 20:49:29 -0230

    Update: 2026-03-24 20:49

--- homeManagerModules/apps/nvim/dap.nix
+++ homeManagerModules/apps/nvim/dap.nix
@@ -1,38 +1,34 @@
-{ pkgs, ... }:
-{
+# -----------------------------------------------------------------------------
+# MODULE: NEOVIM DEBUGGER (DAP)
+# Description: Configuration for lldb (C/C++) and Java debugging.
+# -----------------------------------------------------------------------------
+{pkgs, ...}: {
   programs.nvf.settings.vim = {
-    extraPackages = with pkgs; [
-      lldb
-    ];
+    extraPackages = with pkgs; [lldb];
 
-    debugger = {
-      nvim-dap = {
-        enable = true;
-        ui.enable = true;
-      };
+    debugger.nvim-dap = {
+      enable = true;
+      ui.enable = true;
     };
 
     extraPlugins = with pkgs.vimPlugins; {
       dap-virtual-text = {
         package = nvim-dap-virtual-text;
-        setup = ''
-          require("nvim-dap-virtual-text").setup({})
-        '';
+        setup = "require('nvim-dap-virtual-text').setup({})";
       };
     };
 
     pluginRC.dap-config = ''
+      -- Lua block inside Nix string
       local dap = require("dap")
       local dapui = require("dapui")
 
-      -- C adapter
       dap.adapters.lldb = {
         type = "executable",
         command = "lldb-dap",
         name = "lldb",
       }
 
-      -- C configuration
       dap.configurations.c = {
         {
           name = "Launch",
@@ -43,31 +39,13 @@
           end,
           cwd = "''${workspaceFolder}",
           stopOnEntry = false,
-          args = {},
         },
       }
       dap.configurations.cpp = dap.configurations.c
 
-      -- Java uses jdtls built-in DAP
-      dap.configurations.java = {
-        {
-          name = "Launch Java",
-          type = "java",
-          request = "launch",
-          mainClass = "",
-        },
-      }
-
-      -- Auto open/close dapui
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
     '';
   };
 }
