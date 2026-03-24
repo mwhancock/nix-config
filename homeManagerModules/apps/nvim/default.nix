e745e7aaf03c234ad2c400e03f540348053950d9
mark <mwhancock@proton.me>
2026-03-24 20:48:29 -0230

    Update: 2026-03-24 20:48

--- homeManagerModules/apps/nvim/default.nix
+++ homeManagerModules/apps/nvim/default.nix
@@ -1,21 +1,20 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+# -----------------------------------------------------------------------------
+# MODULE: NEOVIM (NVF) MAIN CONFIGURATION
+# Description: Entry point for the Neovim Flake (nvf) setup.
+# -----------------------------------------------------------------------------
+{...}: {
   imports = [
-    inputs.nvf.homeManagerModules.default
-    ./theme.nix
-    ./lsp.nix
-    ./ui.nix
-    ./editor.nix
-    ./treesitter.nix
-    ./git.nix
-    ./keymaps.nix
     ./ai.nix
     ./dap.nix
-    ./lua.nix
+    ./editor.nix
     ./extra-plugins.nix
+    ./git.nix
+    ./keymaps.nix
+    ./lsp.nix
+    ./lua.nix
+    ./theme.nix
+    ./treesitter.nix
+    ./ui.nix
   ];
 
   programs.nvf = {
@@ -23,45 +22,7 @@
     settings.vim = {
       viAlias = true;
       vimAlias = true;
-
-      options = {
-        number = true;
-        relativenumber = true;
-        tabstop = 4;
-        shiftwidth = 4;
-        expandtab = true;
-        smartindent = true;
-        wrap = false;
-        scrolloff = 8;
-        signcolumn = "yes";
-        updatetime = 50;
-        swapfile = false;
-        termguicolors = true;
-        clipboard = "unnamedplus";
-        colorcolumn = "80";
-      };
-
-      globals = {
-        mapleader = " ";
-        maplocalleader = " ";
-      };
-
-      extraPackages = with pkgs; [
-        lazygit
-        nil
-        nixd
-        ripgrep
-        fd
-        fzf
-        clang-tools
-        pyright
-        black
-        stylua
-        gnumake
-        jdt-language-server
-        nodePackages.bash-language-server
-        nodejs
-      ];
+      useSystemClipboard = true;
     };
   };
 }
